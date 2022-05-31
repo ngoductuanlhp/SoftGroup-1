7291c6a4bb1a94d3113cc9eac3b482758a7fd3b4
ngoductuanlhp <ngoductuanlh@gmail.com>
2022-05-31 17:31:21 +0700

    + add gen knn_graph

---
+++ geodist/shortest_path.py
@@ -0,0 +1,71 @@
+import cudf
+import cugraph
+
+import sys
+
+import numpy as np
+import pickle
+
+from tqdm import tqdm
+import os
+import argparse
+
+try:
+    with open('graph_test.pkl', 'rb') as handle:
+        saved_dict = pickle.load(handle)
+except:
+    import pickle5 as pickle
+    with open('graph_test.pkl', 'rb') as handle:
+        saved_dict = pickle.load(handle)
+
+edges = saved_dict['edges']
+xyz = saved_dict['xyz']
+rgb = saved_dict['rgb']
+pivots_inds = saved_dict['pivots_inds']
+pivots = saved_dict['pivots']
+
+n_pivots = pivots.shape[0]
+gdf = cudf.DataFrame()
+# print(edges.shape)
+gdf['src'] = edges[:, 0].astype(int)
+gdf['dst'] = edges[:, 1].astype(int)
+gdf["data"] = edges[:, 2]
+
+# print(np.max(edges[:, 0]), np.max(edges[:, 1]))
+num_vertice = len(np.unique(edges[:, 0:2]))
+# st = time.time()
+G = cugraph.Graph()
+
+# print('edges', edges.shape, edges[:10, :])
+G.from_cudf_edgelist(gdf, source='src', destination='dst', edge_attr='data')
+
+
+# print(edges.shape)
+
+# geo_distance = np.zeros((128, num_vertice-128)) + 10000
+geo_dict = {}
+for i, ind in enumerate(pivots_inds):
+    # print(ind)
+    df = cugraph.sssp(G, i)
+
+    # print('num_vertices', num_vertices)
+    df = cugraph.filter_unreachable(df)
+    # print(df)
+    # quit()
+    dis = np.array(df['distance'].to_array())
+    vertex = np.array(df['vertex'].to_array())
+    # num_vertices = len(vertex)
+    # 
+    mask = (vertex >= n_pivots)
+    vertex = vertex[mask] - n_pivots
+    dis = dis[mask]
+
+    # geo_dict = 
+    geo_dict[i] = {
+        'vertex': vertex,
+        'dis': dis,
+    }
+
+save_scene_path = os.path.join('path_test.pkl')
+with open(save_scene_path, 'wb') as handle:
+    pickle.dump(geo_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ geodist/vis.py
@@ -0,0 +1,118 @@
+# import pickle
+import numpy as np
+import mayavi.mlab as mlab
+import pickle5 as pickle
+
+
+color_r = np.linspace(0, 1, 5)
+color_g = np.linspace(0, 1, 5)
+color_b = np.linspace(0, 1, 5)
+
+color_rgb = np.meshgrid(color_r, color_g, color_b)
+# print(color_rgb)
+color_rgb = np.stack(color_rgb, axis=-1).reshape(-1, 3)
+# print(color_rgb.shape)
+COLOR_DETECTRON2 = color_rgb
+# scene_name = 'scene0700_01'
+idx = range(0, 256, 1)
+
+# for s in geo_dict.keys():
+
+with open('graph_test.pkl', 'rb') as handle:
+    saved_dict = pickle.load(handle)
+
+with open('path_test.pkl', 'rb') as handle:
+    geo_dist_pkl = pickle.load(handle)
+
+# 
+# scene = scene_info[scene_name]
+
+
+
+
+# print('geo_distance', geo_distance.shape)
+
+edges = saved_dict['edges']
+xyz = saved_dict['xyz']
+rgb = saved_dict['rgb']
+pivots_inds = saved_dict['pivots_inds']
+pivots = saved_dict['pivots']
+# print(query.shape)
+
+min_z = np.min(xyz[:, 2])
+idx = [i for i in idx if xyz[pivots_inds[i],2] > min_z + 0.2]
+
+geo_dist = []
+max_len = xyz.shape[0]
+for i, k in enumerate(geo_dist_pkl.keys()):
+    geo = np.ones((max_len)) * 100
+    geo[geo_dist_pkl[k]['vertex'].astype(np.long)] = geo_dist_pkl[k]['dis']
+    geo_dist.append(geo)
+geo_distance = np.stack(geo_dist, axis=0) # 128, N
+
+
+# print(geo_dist_pkl[idx]['dis'])
+sing_geo = geo_distance[idx] # N
+pivot = pivots[idx]
+
+# sort_inds = np.argsort(sing_geo)
+# top_sort = sort_inds[0:5000]
+# bot_sort = sort_inds[5000:]
+
+# sing_eu = np.square(locs - query).sum(1)
+# sort_inds = np.argsort(sing_eu)
+# top_sort = sort_inds[0:5000]
+# bot_sort = sort_inds[5000:]
+
+
+# locs_filter = locs[top_sort]
+mask = (sing_geo < 1.5) # P x N
+
+mask = (np.sum(mask, 0) >= 1) # N
+
+points_near = xyz[mask] # N
+
+
+points_far = xyz[~mask] 
+
+
+points_pivot_inds = np.ones((max_len)) * -1
+points_pivot_dis = np.ones((max_len)) * 1000
+for i in idx:
+    single_geo = geo_distance[i] # N
+    mask_valid = (single_geo < points_pivot_dis) & (single_geo < 1)
+    # print(mask_valid.shape)
+    points_pivot_dis[mask_valid] = single_geo[mask_valid]
+    points_pivot_inds[mask_valid] = i
+
+
+fig = mlab.figure(figure=None, bgcolor=(1.0, 1.0, 1.0), size=((800, 800)))
+
+mlab.points3d(pivot[:, 0],pivot[:, 1],pivot[:, 2], color=(1,0,0), mode='sphere', scale_factor=0.05, figure=fig)
+
+
+for i in idx:
+    mask_points = (points_pivot_inds == i)
+    points_neighbor = xyz[mask_points]
+    color_p = tuple(COLOR_DETECTRON2[i%COLOR_DETECTRON2.shape[0]])
+    mlab.points3d(points_neighbor[:,0],points_neighbor[:,1],points_neighbor[:,2], color=color_p, mode='sphere', scale_factor=0.02, figure=fig)
+# locs_filter_dist = np.square(locs - query).sum(1)
+# locs_filter_dist_mask = (locs_filter_dist < 2.5)
+# locs_filter = locs[locs_filter_dist_mask, :]
+
+# locs_nofilter = locs[~locs_filter_dist_mask, :]
+
+# mlab.points3d(locs_filter[:,0],locs_filter[:,1],locs_filter[:,2], color=(0,1,0), mode='sphere', scale_factor=0.02, figure=fig)
+# mlab.points3d(locs_nofilter[:,0],locs_nofilter[:,1],locs_nofilter[:,2], color=(0.3,0.3,0.3), mode='sphere', scale_factor=0.02, figure=fig)
+
+
+# mlab.points3d(points_near[:,0],points_near[:,1],points_near[:,2], color=(0,1,0), mode='sphere', scale_factor=0.02, figure=fig)
+
+
+# # locs_out_dist = np.square(locs_out - query).sum(1)
+# # locs_out_dist_mask = (locs_out_dist < 4)
+# # locs_out = locs_out[locs_out_dist_mask, :]
+
+mlab.points3d(points_far[:,0],points_far[:,1],points_far[:,2], color=(171/255.0, 198/255.0, 230/255.0), mode='sphere', scale_factor=0.02, figure=fig)
+
+mlab.show()
