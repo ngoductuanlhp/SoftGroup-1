cda41a5ded91afeaa50862a9305dd6b4b743bab0
ngoductuanlhp <ngoductuanlhp@gmail.com>
2022-06-10 09:29:15 +0000

    # modify save res

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -5,3 +5,6 @@ GPUS=$3
 PORT=${PORT:-29501}
 
 OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT  $(dirname "$0")/test.py $CONFIG $CHECK_POINT --dist ${@:4}
+
+
+CUDA_VISIBLE_DEVICES=0 python3 tools/test.py configs/softgroup_scannet.yaml work_dirs/softgroup_scannet/best.pth
