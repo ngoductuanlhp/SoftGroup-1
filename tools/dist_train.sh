7403f7e4f9d4c17881bf467e71b69dcdfdb95a93
ngoductuanlhp <ngoductuanlhp@gmail.com>
2022-06-17 14:28:59 +0000

    # modify script train

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -8,3 +8,5 @@ OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT $(dirname 
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=$((RANDOM + 10000)) tools/train.py --dist configs/softgroup_scannet_bbox_context.yaml 
 
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=1 torchrun --nproc_per_node=2 --master_port=$((RANDOM + 10000)) tools/train.py --dist configs/softgroup_scannet_bbox_context.yaml
+
+CUDA_VISIBLE_DEVICE=2 python3 tools/train.py configs/softgroup_scannet_bbox_context.yaml
